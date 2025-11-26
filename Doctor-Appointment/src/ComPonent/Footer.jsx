eeae7a3116d8e11a4b3b532bfe36370ac88b4b0f
Arifhossain0181 <arifhossain01816047075@gmail.com>
2025-11-27 00:53:27 +0600

    Update Footer component

--- Doctor-Appointment/src/ComPonent/Footer.jsx
+++ Doctor-Appointment/src/ComPonent/Footer.jsx
@@ -3,33 +3,33 @@ import { assets } from '../assets/assets';
 
 const Footer = () => {
     return (
-        <div className='md:mx-10'>
-            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div className="px-5 md:px-10 bg-white">
+            <div className="flex flex-col sm:grid grid-cols-1 md:grid-cols-[2fr_1fr_1fr] gap-10 my-10 mt-20 text-sm">
+
                 {/* Left Section */}
-                <div>
-                    <img className='mb-5 w-40' src={assets.logo} alt="Logo" />
-                    <p className='w-full md:w-2/3 text-gray-600 leading-6'>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                      
+                <div className="text-center sm:text-left">
+                    <img className="mb-5 w-32 mx-auto sm:mx-0" src={assets.logo} alt="Logo" />
+                    <p className="w-full md:w-2/3 text-gray-600 leading-6 mx-auto sm:mx-0">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
                     </p>
                 </div>
 
                 {/* Center Section - Company */}
-                <div>
-                    <p className='text-xl font-medium mb-5'>COMPANY</p>
-                    <ul className='flex flex-col gap-2 text-gray-600'>
-                        <li>Home</li>
-                        <li>About us</li>
-                        <li>Contact us</li>
-                        <li>Privacy policy</li>
+                <div className="text-center sm:text-left">
+                    <p className="text-lg font-semibold mb-4">COMPANY</p>
+                    <ul className="flex flex-col gap-2 text-gray-600">
+                        <li className="cursor-pointer hover:text-black">Home</li>
+                        <li className="cursor-pointer hover:text-black">About us</li>
+                        <li className="cursor-pointer hover:text-black">Contact us</li>
+                        <li className="cursor-pointer hover:text-black">Privacy policy</li>
                     </ul>
                 </div>
 
                 {/* Right Section - Get in Touch */}
-                <div>
-                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-                    <ul className='flex flex-col gap-2 text-gray-600'>
+                <div className="text-center sm:text-left">
+                    <p className="text-lg font-semibold mb-4">GET IN TOUCH</p>
+                    <ul className="flex flex-col gap-2 text-gray-600">
                         <li>+1-212-456-7890</li>
                         <li>greatstackdev@gmail.com</li>
                     </ul>
@@ -39,7 +39,9 @@ const Footer = () => {
             {/* Copyright */}
             <div>
                 <hr />
-                <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto - All Right Reserved.</p>
+                <p className="py-5 text-sm text-center text-gray-500">
+                    Copyright 2024 © Prescripto - All Rights Reserved.
+                </p>
             </div>
         </div>
     );
